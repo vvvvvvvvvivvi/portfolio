696944dc659e6919a67a7611aaa571c1e702ed50
Vivian <70130389+vvvvvvvvvivvi@users.noreply.github.com>
2025-10-27 23:27:15 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+    plugins: [react()],
+    base: '/portfolio/'   // repo name
+  })
